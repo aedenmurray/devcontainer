2de285ac34383736d0ba479289b99d53273b2150
aedenmurray <aeden@aedenmurray.dev>
2026-08-28 23:56:29 -0600

    update

--- features/just/install.sh
+++ features/just/install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 : "${VERSION:?the 'version' option is required}"
+: "${LSPVERSION:?the 'lspVersion' option is required}"
+arch=$(uname -m)
 
 curl -fsSL https://just.systems/install.sh |
 bash -s -- --tag "$VERSION" --to /usr/local/bin
+
+curl -fsSL "https://github.com/terror/just-lsp/releases/download/$LSPVERSION/just-lsp-$LSPVERSION-$arch-unknown-linux-gnu.tar.gz" |
+tar -xz -C /usr/local/bin --no-same-owner ./just-lsp
